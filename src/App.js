8d5c4f66b33afd866088258b66f48322884487e2
unknown <shukumar-542>
2022-03-05 13:21:00 +0600

    password and email authenticate

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { GoogleAuthProvider } from "firebase/auth";
 import { getAuth, signInWithPopup } from "firebase/auth";
 import { useState } from 'react';
 import { signOut } from "firebase/auth";
+import {createUserWithEmailAndPassword } from "firebase/auth";
 
  
 initializeApp(firebaseConfig);
@@ -15,7 +16,10 @@ function App() {
     isSingIn : false,
     name : '',
     email : '',
+    password : '',
     photo : '',
+    error : '',
+    success : false
   
   });
 
@@ -43,7 +47,7 @@ function App() {
         setUser(singInUser);
 
        
-        console.log(user.photoURL);
+        // console.log(user.photoURL);
         // ...
       })
       .catch(err =>{console.log(err);})
@@ -62,6 +66,57 @@ function App() {
     })
     .catch(err =>{console.log(err);});
   }
+
+  const handleOnBlur =(event) =>{
+   let isFormValid = true;
+   
+   if( event.target.name === 'email'){
+    isFormValid = /\S+@\S+\.\S+/.test(event.target.value);
+    // console.log(isEmailValid);
+   }
+   if(event.target.name === 'password'){
+    const isValidPassword = event.target.value.length > 6 ; 
+     const passwordHasNumber =/^.*[0-9]+.*$/.test(event.target.value);
+     
+     isFormValid = isValidPassword && passwordHasNumber;
+   }
+   if(isFormValid){
+     const newUser = {...user}
+     newUser[event.target.name] = event.target.value;
+     setUser(newUser)
+   }
+  }
+
+  const handleSubmit =(e)=>{
+    console.log(user.email, user.password);
+    if(user.email && user.password){
+      const auth = getAuth();
+  createUserWithEmailAndPassword(auth, user.email, user.password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+    const newUserInfo = {...user}
+    newUserInfo.error = ''
+    newUserInfo.success = true
+    setUser(newUserInfo);
+  })
+  .catch((error) => {
+    const newUserInfo = {...user};
+    newUserInfo.error = error.code;
+    newUserInfo.success = false
+    setUser(newUserInfo);
+    // const errorMessage = error.message;
+    // const errorCode = error.code;
+    // console.log(errorMessage, errorCode);
+    // ..
+  });
+
+    }
+    e.preventDefault();
+   
+  }
+
   return (
     <div className="App">
       {user.isSingIn ? <button onClick={handleSingOut}>Sign out</button>:
@@ -75,8 +130,30 @@ function App() {
           <img src={user.photo} alt=''></img>
         </div>
       }
-      
+      <h1>Authentication</h1>
+    
+   
+     
+   <form onSubmit={handleSubmit}>
+      {/* <input type="text" name="name" onBlur={handleOnBlur} placeholder='Enter your Name' required  /><br/> */}
+      <input type="text" onBlur={handleOnBlur} placeholder='Enter Your Email' name="email" required />
+      <br/>
+      <input type="password" onBlur={handleOnBlur} placeholder='Enter Your Password'  name="password" required />
+      <br/>
+      <input type="Submit" />
+      {/* <input type="button" value="Submit" /> */}
 
+     
+   </form>
+   <p style={{color : 'red'}}>{user.error}</p>
+   {
+     user.success && <p style={{color : 'green'}}>Your Account Is Created</p>
+   }
+   
+
+   
+      
+      
     </div>
   );
 }
